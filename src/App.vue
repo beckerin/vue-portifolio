6a9285ed26ea275cdcdc3a65b51ca4838ca0f2bb
Diego Paiva <paivadiego7797@gmail.com>
2022-12-06 17:40:40 +0000

    Salve

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      a: "Botão",
+    };
+  },
+  methods: {
+    changeTo(val) {
+      this.a = val;
+    },
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <div>
+      <b-button @click="changeTo('Baton')">{{ a }}</b-button>
+    </div>
+  </div>
+</template>
